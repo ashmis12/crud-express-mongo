bceb2c02a4179cd22d8545502fd0bb25913dc1c5
Ashish Mishra <mishraashish1811@gmail.com>
2022-12-15 13:30:08 +0530

    added my fatoora

---
+++ server.js
@@ -0,0 +1,128 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const axios = require('axios');
+var db
+
+// Remember to change YOUR_USERNAME and YOUR_PASSWORD to your username and password! 
+MongoClient.connect('mongodb+srv://dbgod:8J0PMrEpRLh8IJoD@teamfacex.ddmcn.mongodb.net/scheduler?authSource=admin&replicaSet=atlas-2d1eur-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', (err, database) => {
+  if (err) return console.log(err)
+  db = database.db('star-wars-quotes')
+  app.listen(process.env.PORT || 6500, () => {
+    console.log('listening on 6500')
+  })
+})
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.get('/initiate-session', async (req, res) => {
+  const config = {
+    method: 'post',
+    url: 'https://apitest.myfatoorah.com/v2/InitiateSession',
+    headers: {
+      Authorization: "Bearer rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"
+    },
+    data: {
+      CustomerIdentifier: req.query.userId,
+      CountryCode: "ARE"
+    }
+  };
+  console.log(config);
+  let result = await axios(config).then((resp) => {
+    console.log("==================",resp?.data);  
+    return res.status(200).send(resp.Data);
+  }).catch((err) => {
+    console.log(err);
+  });
+})
+
+
+app.get('/initiate-payment', async (req, res) => {
+  const config = {
+    method: 'post',
+    url: 'https://apitest.myfatoorah.com/v2/InitiatePayment',
+    headers: {
+      Authorization: "Bearer rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"
+    },
+    data: { InvoiceAmount: 100, CurrencyIso: 'AED' }
+  };
+  console.log(config);
+  const result = await axios(config).then((resp) => {
+    console.log("==================",resp?.data, (resp.data.Data));
+    return res.status(200).send(resp.data?.Data);
+  }).catch((err) => {
+    console.log(err);
+  });
+  // console.log(result);
+  // db.collection('quotes').find().toArray((err, result) => {
+    // if (err) return console.log(err)
+  // })
+})
+
+
+app.get('/execute-payment', async (req, res) => {
+  const config = {
+    method: 'post',
+    url: 'https://apitest.myfatoorah.com/v2/InitiatePayment',
+    headers: {
+      Authorization: "Bearer rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL"
+    },
+    data: { SessionId: req.query.sessionId, InvoiceValue: 10 }
+  };
+  console.log(config);
+  const result = await axios(config).then((resp) => {
+    console.log("==================",res?.data, (res.data.Data));  
+    return res.status(200).send(resp.Data);
+  }).catch((err) => {
+    console.log(err);
+  });
+})
+
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('A darth vadar quote got deleted')
+  })
+})
+
+
+
+// 0bf0be13-6cea-4c2d-9a0b-c18ca975f397
+
+
